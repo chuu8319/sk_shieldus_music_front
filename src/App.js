f68a78db2fd28257e544839360e90e74ddd790d7
chuu8319 <chuu8319@gmail.com>
2024-07-25 17:20:10 +0900

    Reapply "first commit"
    This reverts commit 0c775d94de6d5162427037a5e31560fe02c8c047.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import MusicList from './music/MusicList';
+import MusicWrite from './music/MusicWrite';
+import MusicDetail from './music/MusicDetail';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<MusicList />} />
+        <Route path="/list" element={<MusicList />} />
+        <Route path="/write" element={<MusicWrite />} />
+        <Route path="/detail/:musicId" element={<MusicDetail />} />  
+      </Routes>
+    </>
+  )
 }
 
 export default App;
